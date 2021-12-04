ad4dd38a5be394ed7a296c7ba6fbbe89811c37b0
Derrick Beining <derrick.beining@gmail.com>
2021-12-03 23:32:23 -0500

    day 1 complete

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -10,10 +10,13 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
-
+import qualified Data.Monoid as Monoid
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text ( Parser )
+import qualified Data.Attoparsec.Text as Parser
 import Data.Void
+import Data.Function ((&))
+import Data.Functor ((<&>))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +24,48 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  Parser.sepBy Parser.double Parser.endOfLine
+    <&> fmap truncate
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+-- partA :: Input -> OutputA
+-- partA = go 0
+--  where
+--   go count depths =
+--     case depths of
+--       [] -> count
+--       [x] -> count
+--       x : y : tail ->
+--         if y > x
+--           then go (count + 1) (y : tail)
+--           else go count (y : tail)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA =
+  snd . foldl' countSuccessiveIncreases (Nothing, 0)
+ where
+  countSuccessiveIncreases :: (Maybe Int, Int) -> Int -> (Maybe Int, Int)
+  countSuccessiveIncreases (prev, count) val =
+    ( Just val
+    , maybe 0 (\prev' -> if val > prev' then count + 1 else count) prev
+    )
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = go 0
+ where
+  go count depths =
+    case depths of
+      a : b : c : d : tail ->
+        if b + c + d > a + b + c
+          then go (count + 1) (b : c : d : tail)
+          else go count (b : c : d : tail)
+      _ -> count
