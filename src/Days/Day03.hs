5b294642237271bbaf4eed42fd42a4de60bcc254
Derrick Beining <derrick.beining@gmail.com>
2021-12-05 21:05:41 -0500

    day 3 complete

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,39 +1,99 @@
-module Days.Day03 (runDay) where
+module Days.Day03 (runDay, binToDec) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
+import Data.List ()
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
+import Data.Vector (Vector, (!?), (//))
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import Data.Attoparsec.Text (Parser)
+import qualified Data.Attoparsec.Text as Parser
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import qualified Data.Maybe as Maybe
+import Data.Void (Void)
+import qualified Program.RunDay as R (Day, runDay)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  Parser.sepBy binParser Parser.endOfLine
+    <&> Vec.fromList
+ where
+  binParser =
+    fmap Vec.fromList $
+      Parser.many' $
+        Parser.choice
+          [ False <$ Parser.char '0'
+          , True <$ Parser.char '1'
+          ]
 
 ------------ TYPES ------------
-type Input = Void
+type Binary = Vector Bool
+type Input = Vector Binary
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+
+binToDec :: Binary -> Int
+binToDec = Vec.ifoldr convert 0 . Vec.reverse
+ where
+  convert ix bit acc =
+    acc + ((2 ^ ix) * fromEnum bit)
+
+calculateMostCommonBits :: Vector Binary -> Vector Bool
+calculateMostCommonBits bins =
+  Vec.ifoldl' reduce Vec.empty bins
+    <&> Vec.partition id
+    <&> (\(ones, zeros) -> length ones >= length zeros)
+ where
+  reduce :: Vector Binary -> Int -> Binary -> Vector Binary
+  reduce acc binsIx bin
+    | null acc = bin <&> Vec.singleton
+    | otherwise = Vec.ifoldl' go acc bin
+
+  go :: Vector Binary -> Int -> Bool -> Vector Binary
+  go accum binIx bit =
+    accum // [(binIx, appended)]
+   where
+    appended =
+      fromMaybe Vec.empty (accum !? binIx)
+        `Vec.snoc` bit
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA bins =
+  binToDec gammaRate * binToDec epsilonRate
+ where
+  gammaRate = calculateMostCommonBits bins
+  epsilonRate = not <$> gammaRate
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB bins =
+  fromMaybe 0 $ Just (*) <*> oxyGenRating <*> c02ScrubRating
+ where
+  oxyGenRating =
+    binToDec <$> findRating calculateMostCommonBits bins 0
+  c02ScrubRating =
+    binToDec <$> findRating (fmap not . calculateMostCommonBits) bins 0
+
+  findRating :: (Vector Binary -> Binary) -> Vector Binary -> Int -> Maybe Binary
+  findRating method bins ix =
+    let mostCommonBits = method bins
+        remainingBins =
+          case Vec.filter (\bin -> bin !? ix == mostCommonBits !? ix) bins of
+            filtered | null filtered -> Vec.catMaybes $ Vec.singleton (bins !? (length bins - 1))
+            filtered -> filtered
+     in if length remainingBins == 1
+          then remainingBins !? 0
+          else findRating method remainingBins (ix + 1)
