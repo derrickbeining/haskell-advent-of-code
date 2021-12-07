c0fbb06a69428830b3a81ad2c2b32113ba20c076
Derrick Beining <derrick.beining@gmail.com>
2021-12-07 01:59:21 -0500

    day 4 complete

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,39 +1,218 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Days.Day04 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
+import Control.Monad.State (State)
+import qualified Control.Monad.State as MonadState
+import Data.Attoparsec.Text (
+  Parser,
+  char,
+  double,
+  endOfLine,
+  many',
+  manyTill',
+  sepBy,
+  string,
+ )
+import Data.List (find)
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import qualified Data.List.NonEmpty as NEL
+import Data.Vector (Vector, (!?))
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import Data.Word (Word)
+import qualified GHC.Base as NEL
+import qualified Program.RunDay as R (Day, runDay)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  lottoNums <-
+    lottoNumsParser
+      >>= maybe (fail "No numbers found") pure . NEL.nonEmpty
+  boards <-
+    boardParser `sepBy` endOfLine
+      >>= maybe (fail "No boards found") pure . NEL.nonEmpty
+  pure (lottoNums, boards)
+ where
+  twoNewlines = endOfLine >> endOfLine
+
+  lottoNumsParser = flip manyTill' twoNewlines $ do
+    n <- truncate <$> double :: Parser Word
+    _ <- many' $ string ","
+    pure n
+
+  boardParser = do
+    a <- rowParser <* endOfLine
+    b <- rowParser <* endOfLine
+    c <- rowParser <* endOfLine
+    d <- rowParser <* endOfLine
+    e <- rowParser <* endOfLine
+    pure $ Board a b c d e
+
+  rowParser = do
+    a <- many' (char ' ') *> (truncate <$> double) <* many' (char ' ')
+    b <- many' (char ' ') *> (truncate <$> double) <* many' (char ' ')
+    c <- many' (char ' ') *> (truncate <$> double) <* many' (char ' ')
+    d <- many' (char ' ') *> (truncate <$> double) <* many' (char ' ')
+    e <- many' (char ' ') *> (truncate <$> double) <* many' (char ' ')
+    pure $
+      Row
+        (UnmarkedCell a)
+        (UnmarkedCell b)
+        (UnmarkedCell c)
+        (UnmarkedCell d)
+        (UnmarkedCell e)
 
 ------------ TYPES ------------
-type Input = Void
+type BingoNums = NEL.NonEmpty Word
 
-type OutputA = Void
+type BingoBoard = Board (Row Cell)
 
-type OutputB = Void
+type BingoBoards = NEL.NonEmpty BingoBoard
+
+data Board a = Board a a a a a
+  deriving
+    ( Show
+    , Functor
+    , Foldable
+    )
+
+data Row a = Row a a a a a
+  deriving
+    ( Show
+    , Functor
+    , Foldable
+    )
+
+data Cell
+  = UnmarkedCell Word
+  | MarkedCell Word
+  deriving
+    ( Show
+    )
+
+isMarkedCell :: Cell -> Bool
+isMarkedCell (UnmarkedCell _) = False
+isMarkedCell (MarkedCell _) = True
+
+newtype FinalScore = FinalScore {unFinalScore :: Word}
+  deriving
+    ( Show
+    )
+
+type Input = (BingoNums, BingoBoards)
+
+type OutputA = FinalScore
+
+type OutputB = FinalScore
+
+data BingoState = BingoState
+  { winners :: Vector (Word, BingoBoard)
+  , numbers :: [Word]
+  , boards :: Maybe BingoBoards
+  }
+
+type BingoApp =
+  State BingoState FinalScore
+
+runBingoApp :: BingoState -> BingoApp -> FinalScore
+runBingoApp init app = MonadState.evalState app init
+
+updateBoards :: Word -> BingoBoards -> BingoBoards
+updateBoards numberDrawn =
+  fmap (updateRows numberDrawn)
+
+updateRows :: Word -> BingoBoard -> BingoBoard
+updateRows n =
+  fmap (updateRow n)
+
+updateRow :: Word -> Row Cell -> Row Cell
+updateRow n =
+  fmap (updateCell n)
+
+updateCell :: Word -> Cell -> Cell
+updateCell n cell =
+  case cell of
+    UnmarkedCell m | n == m -> MarkedCell m
+    unchanged -> unchanged
+
+isWinner :: Foldable t => t (Row Cell) -> Bool
+isWinner board =
+  any (all isMarkedCell) board
+    || all (isMarkedCellAt 0) board
+    || all (isMarkedCellAt 1) board
+    || all (isMarkedCellAt 2) board
+    || all (isMarkedCellAt 3) board
+    || all (isMarkedCellAt 4) board
+
+isMarkedCellAt :: (Eq a, Num a) => a -> Row Cell -> Bool
+isMarkedCellAt 0 (Row a b c d e) = isMarkedCell a
+isMarkedCellAt 1 (Row a b c d e) = isMarkedCell b
+isMarkedCellAt 2 (Row a b c d e) = isMarkedCell c
+isMarkedCellAt 3 (Row a b c d e) = isMarkedCell d
+isMarkedCellAt 4 (Row a b c d e) = isMarkedCell e
+isMarkedCellAt _ _ = False
+
+calculateFinalScore :: (Foldable t1, Foldable t2) => Word -> t1 (t2 Cell) -> FinalScore
+calculateFinalScore n board =
+  FinalScore $ foldr foldRow 0 board * n
+ where
+  foldRow row sumSoFar =
+    foldr sumUnmarkedCells sumSoFar row
+
+sumUnmarkedCells :: Cell -> Word -> Word
+sumUnmarkedCells (UnmarkedCell m) sumSoFar = sumSoFar + m
+sumUnmarkedCells (MarkedCell _) sumSoFar = sumSoFar
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (bingoNums, bingoBoards) =
+  case find isWinner updatedBoards of
+    Nothing ->
+      case NEL.nonEmpty remainingNums of
+        Nothing -> FinalScore 0
+        Just nums -> partA (nums, updatedBoards)
+    Just winner ->
+      calculateFinalScore num winner
+ where
+  num :| remainingNums = bingoNums
+
+  updatedBoards = updateBoards num bingoBoards
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (bingoNums, bingoBoards) =
+  MonadState.evalState bingoApp initialState
+ where
+  initialState =
+    BingoState Vec.empty (NEL.toList bingoNums) (Just bingoBoards)
+
+  bingoApp :: BingoApp
+  bingoApp = do
+    state@BingoState{winners, numbers, boards} <- MonadState.get
+
+    case (numbers, boards) of
+      ([], _) -> case winners !? (length winners - 1) of
+        Nothing -> pure $ FinalScore 0
+        Just (num, lastWinner) -> pure $ calculateFinalScore num lastWinner
+      --
+      (_, Nothing) -> case winners !? (length winners - 1) of
+        Nothing -> pure $ FinalScore 0
+        Just (num, lastWinner) -> pure $ calculateFinalScore num lastWinner
+      --
+      (numberDrawn : remainingNumbers, Just boards') -> do
+        let updatedBoards = updateBoards numberDrawn boards'
+            (newWinners, notWinners) = NEL.partition isWinner updatedBoards
+
+        MonadState.put
+          ( state
+              { winners = winners <> ((numberDrawn,) <$> Vec.fromList newWinners)
+              , numbers = remainingNumbers
+              , boards = NEL.nonEmpty notWinners
+              }
+          )
+
+        bingoApp
